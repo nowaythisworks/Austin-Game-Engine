d60570fd78a3ada8725b155dd07514b1488e46ca
Austin <66288732+nowaythisworks@users.noreply.github.com>
2023-03-13 03:54:55 -0700

    Basic Saving and Build System

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ function createWindow() {
         width: 1366,
         height: 768,
         webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
             preload: path.join(__dirname, 'preload.js')
         },
         autoHideMenuBar: true
@@ -18,7 +20,23 @@ function createWindow() {
     mainWindow.loadFile('index.html')
 
     // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
+
+    var ipc = require('electron').ipcMain;
+    ipc.on('save-file', function (event, arg) {
+        var fs = require('fs');
+        // if there is no "saves" directory, create it
+        if (!fs.existsSync('./saves')) {
+            fs.mkdirSync('./saves');
+        }
+        // save the arg (JSON string) to .json file
+        fs.writeFile('./saves/savedata.json', arg, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+    });
 }
 
 // This method will be called when Electron has finished
